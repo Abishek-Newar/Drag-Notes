7e7c9df3b811a8988605dfe26c45cbb6503edaf6
abishejk1872 <abisheknewar15@gmail.com>
2024-01-14 16:29:17 +0530

    drag-notes-project

---
+++ drag-notes-project/src/components/Foreground.jsx
@@ -0,0 +1,8 @@
+import Card from './Card'
+
+export default function Foreground(){
+    return <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+        <Foreground />
+        <Card />
+    </div>
+}
